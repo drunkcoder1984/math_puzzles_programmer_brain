bfe2de3ee77d6d9b6a0612770f76e69b40a1a40b
drunkcoder1984 <drunkcoder1984@gmail.com>
2023-04-04 15:18:09 +0900

    問02を回答

---
+++ simple_and_fast_code/q02.php
@@ -0,0 +1,61 @@
+<?php
+$operator = ['*', ''];
+$useOps = [];
+
+for ($i = 1000; $i < 10000; ++$i) {
+    $num = sprintf('%d', $i);
+    $revnum = strrev($num);
+    foreach ($operator as $useOps[0]) {
+        foreach ($operator as $useOps[1]) {
+            foreach ($operator as $useOps[2]) {
+
+                if (!checkUseOperators($useOps)) {
+                    continue;
+                }
+
+                $value = '';
+                $formula = '';
+                for ($j = 0; $j < 4; ++$j) {
+                    $value .= $num[$j];
+
+                    if (isset($useOps[$j])) {
+                        if ('' === $useOps[$j]) {
+                            continue;
+                        }
+
+                        $formula .= (int)$value . $useOps[$j];
+                    } else {
+                        $formula .= (int)$value;
+                    }
+
+                    $value = '';
+                }
+
+                eval('$result = ' . $formula . ';');
+                if (!is_int($result)) {
+                    continue;
+                }
+
+                if (sprintf('%d', $result) === $revnum) {
+                    echo "----------------------\n";
+                    echo $num . "\n";
+                    echo $formula . "\n";
+                }
+
+            }
+        }
+    }
+}
+
+/**
+ * 四則演算が最低1つ以上使われるか
+ */
+function checkUseOperators(array $useOps): bool
+{
+    $checkOps = array_unique($useOps);
+    if (1 < count($checkOps)) {
+        return true;
+    }
+
+    return '' != $checkOps[0];
+}
