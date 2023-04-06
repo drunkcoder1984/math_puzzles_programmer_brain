04b81779bd495383c4e04f2e3b869883b35085c7
drunkcoder1984 <drunkcoder1984@gmail.com>
2023-04-06 12:13:22 +0900

    問04を回答

---
+++ simple_and_fast_code/q04.php
@@ -0,0 +1,22 @@
+<?php
+echo cutbar(20, 3, 1) . "\n";
+echo cutbar(100, 5, 1) . "\n";
+
+/**
+ * @param int $len 棒の長さ
+ * @param int $num 人数
+ * @param int $cutedLen 人数
+ * @return int 切り分け回数
+ */
+function cutbar(int $len, int $num, int $cutedLen): int
+{
+    if ($cutedLen >= $len) {
+        return 0;
+    } elseif ($cutedLen < $num) {
+        $nextCutedLen = $cutedLen * 2;
+    } else {
+        $nextCutedLen = $cutedLen + $num;
+    }
+
+    return 1 + cutbar($len, $num, $nextCutedLen);
+}
