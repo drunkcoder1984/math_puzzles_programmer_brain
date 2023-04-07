09c042a66a26e015a5981d1a3de3cb020bdae7af
drunkcoder1984 <drunkcoder1984@gmail.com>
2023-04-07 18:44:16 +0900

    問06回答

---
+++ simple_and_fast_code/q06.php
@@ -0,0 +1,23 @@
+<?php
+$result = 0;
+for ($i = 2; $i <= 10000; $i += 2) {
+    if (collatzConjecture($i)) {
+        ++$result;
+    }
+}
+
+echo $result . "\n";
+
+/**
+ * @param int $initValue
+ * @return bool
+ */
+function collatzConjecture(int $initValue): bool
+{
+    $value = $initValue * 3 + 1;
+    while ($value != $initValue && $value != 1) {
+        $value = $value % 2 == 0 ? $value >> 1 : $value * 3 + 1;
+    }
+
+    return $value == $initValue;
+}
