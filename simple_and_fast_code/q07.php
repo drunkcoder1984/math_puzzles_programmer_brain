cce55af56ede3d602a433b944f3b9e88c5bb9626
drunkcoder1984 <drunkcoder1984@gmail.com>
2023-04-09 13:41:37 +0900

    問07回答を修正

--- simple_and_fast_code/q07.php
+++ simple_and_fast_code/q07.php
@@ -1,6 +1,7 @@
 <?php
 for ($date = "19641010"; $date <= "20200724"; $date = date('Ymd', strtotime($date . "+1day"))) {
-    if (bindec(strrev(decbin($date))) == $date) {
-        echo $date . "\n";
+    if (0 === $date % 2 || bindec(strrev(decbin($date))) != $date) {
+        continue;
     }
+    echo $date . "\n";
 }
