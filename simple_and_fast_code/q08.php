b843ccfa95afb460d93bc738adcacd3773f98c02
drunkcoder1984 <drunkcoder1984@gmail.com>
2023-04-10 13:54:56 +0900

    問08修正

--- simple_and_fast_code/q08.php
+++ simple_and_fast_code/q08.php
@@ -1,68 +1,29 @@
 <?php
 define("MAX_MOVE_NUM", 12);
-define("FLAG_UP", 1);
-define("FLAG_RIGHT", 2);
-define("FLAG_DOWN", 4);
-define("FLAG_LEFT", 8);
-define("FLAG_ALL", FLAG_UP | FLAG_RIGHT | FLAG_DOWN | FLAG_LEFT);
+define("MOVE_AXES", [[0, 1], [0, -1], [1, 0], [-1, 0]]);
 
-$maxMoveNum = 12;
-$coordinates = [0 => [0 => 0]];
-$moveCoordinates = [[0, 0]];
-$patternNum = 0;
-$moveNum = 0;
-$x = 0;
-$y = 0;
+echo move([[0, 0]]) . "\n";
 
-while (true) {
-    if (0 == $moveNum && FLAG_ALL == $coordinates[$x][$y]) {
-        break;
+/**
+ * @param array<int, array<string, int>> $logs
+ * @return int
+ */
+function move(array $logs): int
+{
+    $len = count($logs);
+    if ($len > MAX_MOVE_NUM) {
+        return 1;
     }
 
-    switch (true) {
-        case !($coordinates[$x][$y] & FLAG_DOWN) && !isset($coordinates[$x][$y + 1]):
-            $coordinates[$x][$y] |= FLAG_DOWN;
-            $coordinates[$x][$y + 1] = FLAG_UP;
-            ++$y;
-            break;
-
-        case !($coordinates[$x][$y] & FLAG_RIGHT) && !isset($coordinates[$x + 1][$y]):
-            $coordinates[$x][$y] |= FLAG_RIGHT;
-            $coordinates[$x + 1][$y] = FLAG_LEFT;
-            ++$x;
-            break;
-
-        case !($coordinates[$x][$y] & FLAG_LEFT) && !isset($coordinates[$x - 1][$y]):
-            $coordinates[$x][$y] |= FLAG_LEFT;
-            $coordinates[$x - 1][$y] = FLAG_RIGHT;
-            --$x;
-            break;
-
-        case !($coordinates[$x][$y] & FLAG_UP) && !isset($coordinates[$x][$y - 1]):
-            $coordinates[$x][$y] |= FLAG_UP;
-            $coordinates[$x][$y - 1] = FLAG_DOWN;
-            --$y;
-            break;
-
-        default:
-            // ここは途中で進めなくなった場合
-            unset($moveCoordinates[$moveNum]);
-            unset($coordinates[$x][$y]);
-            --$moveNum;
-            [$x, $y] = $moveCoordinates[$moveNum];
-            continue 2;
+    $count = 0;
+    $coordinate = $logs[$len - 1];
+    foreach (MOVE_AXES as $axes) {
+        $nextCoordinate = [$coordinate[0] + $axes[0], $coordinate[1] + $axes[1]];
+        if (in_array($nextCoordinate, $logs)) {
+            continue;
+        }
+        $count += move($logs + [$len => $nextCoordinate]);
     }
-    ++$moveNum;
 
-    if ($moveNum < MAX_MOVE_NUM) {
-        $moveCoordinates[$moveNum] = [$x, $y];
-
-    } else {
-        ++$patternNum;
-        unset($moveCoordinates[$moveNum]);
-        unset($coordinates[$x][$y]);
-        --$moveNum;
-        [$x, $y] = $moveCoordinates[$moveNum];
-    }
+    return $count;
 }
-echo $patternNum . "\n";
