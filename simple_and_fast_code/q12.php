156f2e10d6122b8dfbcf8ebe29e6d324948c9c2c
drunkcoder1984 <drunkcoder1984@gmail.com>
2023-04-14 21:30:19 +0900

    問12回答

---
+++ simple_and_fast_code/q12.php
@@ -0,0 +1,24 @@
+<?php
+$result = [];
+$num = 2;
+while (2 > count($result)) {
+    $sqrt = sprintf('%.10f', bcsqrt($num, 20));
+    $splitSqrts = explode('.', $sqrt);
+
+    if (!isset($result[0]) && check(0 == (int)$splitSqrts[1] ? $splitSqrts[0] : $splitSqrts[0] . $splitSqrts[1])) {
+        $result[0] = $num;
+    }
+
+    if (!isset($result[1]) && check($splitSqrts[1])) {
+        $result[1] = $num;
+    }
+    ++$num;
+}
+
+echo $result[0] . "\n";
+echo $result[1] . "\n";
+
+function check(string $str): bool
+{
+    return count(array_unique(str_split(substr($str, 0, 10)))) >= 10;
+}
