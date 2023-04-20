edcf7b99b5cbcb0c51e2b61ebba36301decd72e9
drunkcoder1984 <drunkcoder1984@gmail.com>
2023-04-20 23:34:05 +0900

    問17回答

--- simple_and_fast_code/q17.php
+++ simple_and_fast_code/q17.php
@@ -1,23 +1,8 @@
 <?php
-echo check(30) . "\n";
-
-/**
- * @param int $max
- * @param array<int, int> $ary
- * @return int
- */
-function check(int $max, array $ary = []): int
-{
-    $cnt = count($ary);
-    if ($cnt >= $max) {
-        return 1;
-    }
-
-    $num = 0;
-    $checks = 0 == $cnt || 0 == $ary[$cnt - 1] ? [0, 1] : [0];
-    foreach ($checks as $v) {
-        $num += check($max, $ary + [$cnt => $v]);
-    }
-
-    return $num;
+$b = 1;
+$g = 0;
+for ($i = 0; $i < 30; ++$i) {
+    [$b, $g] = [$b + $g, $b];
 }
+
+echo ($b + $g) . "\n";
