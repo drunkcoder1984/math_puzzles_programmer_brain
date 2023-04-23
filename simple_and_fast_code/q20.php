b8e896dc2e336f6b8e2a2ebba41a744bf8f29c18
drunkcoder1984 <drunkcoder1984@gmail.com>
2023-04-23 21:26:00 +0900

    問20修正

--- simple_and_fast_code/q20.php
+++ simple_and_fast_code/q20.php
@@ -1,5 +1,4 @@
 <?php
-require_once(dirname(__FILE__) . "/../lib/array_util.php");
 $magicSquare = [
     1, 14, 14, 4,
     11, 7, 6, 9,
@@ -7,16 +6,15 @@ $magicSquare = [
     13, 2, 3, 15
 ];
 
-$checks = [];
-$cnt = count($magicSquare);
-for ($i = 1; $i <= $cnt; ++$i) {
-    $comb = ArrayUtil::combination($magicSquare, $i);
-    foreach ($comb as $ary) {
-        $sum = array_sum($ary);
-        $checks[$sum] = $checks[$sum] ?? 0;
-        ++$checks[$sum];
+$sumAll = array_sum($magicSquare);
+$sums = array_fill(0, $sumAll + 1, 0);
+$sums[0] = 1;
+foreach ($magicSquare as $v) {
+    for ($i = $sumAll - $v; 0 <= $i; --$i) {
+        $sums[$i + $v] += $sums[$i];
     }
 }
-arsort($checks);
-$keys = array_keys($checks);
+
+arsort($sums);
+$keys = array_keys($sums);
 echo array_shift($keys) . "\n";
