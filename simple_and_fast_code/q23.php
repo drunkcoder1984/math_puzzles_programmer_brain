87def9f65c5707b38346c445f39ca49a0008e6de
drunkcoder1984 <drunkcoder1984@gmail.com>
2023-04-26 14:32:00 +0900

    問23修正

--- simple_and_fast_code/q23.php
+++ simple_and_fast_code/q23.php
@@ -1,16 +1,28 @@
 <?php
 echo check(10, 24) . "\n";
 
-function check(int $coin, int $num): int
+/**
+ * @param int $coin
+ * @param int $num
+ * @param array $memo
+ * @return int
+ */
+function check(int $coin, int $num, array &$memo = []): int
 {
+    if (isset($memo[$coin][$num])) {
+        return $memo[$coin][$num];
+    }
+
     if (0 >= $num || 0 >= $coin) {
-        return 0 < $coin ? 1 : 0;
+        $memo[$coin][$num] = 0 < $coin ? 1 : 0;
+        return $memo[$coin][$num];
     }
 
     $count = 0;
     foreach ([1, -1] as $v) {
-        $count += check($coin + $v, $num - 1);
+        $count += check($coin + $v, $num - 1, $memo);
     }
 
-    return $count;
+    $memo[$coin][$num] = $count;
+    return $memo[$coin][$num];
 }
