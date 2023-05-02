a5f6d88da440eefb09ef5afcbab06fbda516b148
drunkcoder1984 <drunkcoder1984@gmail.com>
2023-05-02 15:11:31 +0900

    問28修正

--- simple_and_fast_code/q28.php
+++ simple_and_fast_code/q28.php
@@ -1,39 +1,30 @@
 <?php
 $clubs = [
-    [11000, 40],
-    [8000, 30],
-    [400, 24],
-    [800, 20],
-    [900, 14],
-    [1800, 16],
-    [1000, 15],
-    [7000, 40],
-    [100, 10],
-    [300, 12],
+    [11000, 40], [8000, 30], [400, 24], [800, 20], [900, 14],
+    [1800, 16], [1000, 15], [7000, 40], [100, 10], [300, 12],
 ];
 
 echo check($clubs, 150) . PHP_EOL;
 
 /**
  * @param array<int, array<0: int, 1: int>> $clubs
- * @param int $maxNum
- * @param int $num
- * @param int $area
+ * @param int $max
  * @return int
  */
-function check(array $clubs, int $maxNum, int $num = 0, int $area = 0): int
+function check(array $clubs, int $max): int
 {
-    $maxArea = $area;
-    foreach ($clubs as $k => $v) {
-        $nNum = $num + $v[1];
-        if ($nNum > $maxNum) {
-            continue;
+    $memo = [];
+    foreach ($clubs as [$area, $num]) {
+        foreach ($memo as $k => $v) {
+            $nNum = $num + $k;
+            if ($nNum > $max) {
+                continue;
+            }
+            $nArea = $area + $v;
+            $memo[$nNum] = isset($memo[$nNum]) ? max($memo[$nNum], $nArea) : $nArea;
         }
-
-        $nClubs = $clubs;
-        unset($nClubs[$k]);
-        $maxArea = max(check($nClubs, $maxNum, $nNum, $area + $v[0]), $maxArea);
+        $memo[$num] = isset($memo[$num]) ? max($memo[$num], $area) : $area;
     }
 
-    return $maxArea;
+    return max($memo);
 }
