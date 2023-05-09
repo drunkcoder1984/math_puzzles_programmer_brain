3eb263cfd0471cee6d53863ec614d283f11c9aa5
drunkcoder1984 <drunkcoder1984@gmail.com>
2023-05-09 22:55:19 +0900

    問35回答

---
+++ simple_and_fast_code/q35.php
@@ -0,0 +1,42 @@
+<?php
+require_once(dirname(__FILE__) . "/../lib/array_util.php");
+
+define("_LEN_", 6);
+define("_MOVES_", ArrayUtil::product([[1, 0], [0, 1]], [[-1, 0], [0, -1]]));
+
+echo check([0, 0], [_LEN_, _LEN_]) . PHP_EOL;
+
+/**
+ * @param array<0: int, 1: int> $mPos
+ * @param array<0: int, 1: int> $wPos
+ * @param int $meet
+ * @return int
+ */
+function check(array $mPos, array $wPos, int $meet = 0): int
+{
+    if (_LEN_ < $mPos[0] || _LEN_ < $mPos[1] || 0 > $wPos[0] || 0 > $wPos[1]) {
+        return 0;
+    } elseif ([_LEN_, _LEN_] === $mPos) {
+        return 2 <= $meet ? 1 : 0;
+    }
+
+    $meet += $mPos[0] == $wPos[0] ? 1 : 0 ;
+    $meet += $mPos[1] == $wPos[1] ? 1 : 0 ;
+
+    $cnt = 0;
+    foreach (_MOVES_ as [$mMove, $wMove]) {
+        $cnt += check(move($mPos, $mMove), move($wPos, $wMove), $meet);
+    }
+
+    return $cnt;
+}
+
+/**
+ * @param array<0: int, 1: int> $pos
+ * @param array<0: int, 1: int> $move
+ * @return array<0: int, 1: int>
+ */
+function move(array $pos, array $move): array
+{
+    return [$pos[0] + $move[0], $pos[1] + $move[1]];
+}
