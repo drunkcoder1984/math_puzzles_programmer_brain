58c0305c1f0785e11a36e3a4eb16c0f203cd4a53
drunkcoder1984 <drunkcoder1984@gmail.com>
2023-05-11 23:58:41 +0900

    問36修正

--- simple_and_fast_code/q36.php
+++ simple_and_fast_code/q36.php
@@ -1,22 +1,31 @@
 <?php
-$map = array_filter(range(1, 50), fn($v) => ($v & 1) && $v % 5 != 0 && 13 != $v);
-$result = [];
-for ($i = 1; [] !== $map; ++$i) {
-    $n = sprintf("%b", $i) * 7;
-    foreach ($map as $k => $v) {
-        if (0 != $n % $v) {
-            continue;
-        }
-
-        unset($map[$k]);
-        if ($n != strrev($n) || false === strstr($n, 0)) {
-            continue;
-        }
-        $result[$v] = $n;
-    }
+foreach (check(50) as $k => $v) {
+    echo $k . ": " . $v . PHP_EOL;
 }
 
-ksort($result);
-foreach ($result as $k => $v) {
-    echo $k . ": " . $v . PHP_EOL;
+/**
+ * @param int $num
+ * @return array<int, int>
+ */
+function check(int $num): array
+{
+    $map = array_filter(range(1, $num), fn($v) => ($v & 1) && $v % 5 != 0 && 13 != $v);
+    $result = [];
+    for ($i = 1; [] !== $map; ++$i) {
+        $n = sprintf("%b", $i) * 7;
+        foreach ($map as $k => $v) {
+            if (0 != $n % $v) {
+                continue;
+            }
+
+            unset($map[$k]);
+            if ($n != strrev($n) || false === strstr($n, 0)) {
+                continue;
+            }
+            $result[$v] = $n;
+        }
+    }
+
+    ksort($result);
+    return $result;
 }
